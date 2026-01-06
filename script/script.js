d21e05a15a3c8ed395c2828bd8d7a1cc9c27479b
Vinn1Ramos <viniciusdesessardsramos@gmail.com>
2026-01-05 23:46:16 -0300

    att v1.02
    function login

--- script/script.js
+++ script/script.js
@@ -7,9 +7,3 @@ window.addEventListener('scroll', function() {
         nav.style.backgroundColor = 'rgba(0, 0, 0, 0.9)';
     }
 });
-
-function abrirAgendamento() {
-    // Abre o chat do WhatsApp em nova aba com mensagem pré-preenchida
-    const waUrl = "https://wa.me/5132472310?text=Ol%C3%A1!%20Gostaria%20de%20agendar%20um%20hor%C3%A1rio%20na%20Ramos%20Barbearia.%20Meu%20nome%20%3A%20%5Bseu%20nome%5D.";
-    window.open(waUrl, '_blank');
-}
