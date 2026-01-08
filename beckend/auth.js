94bf4b978e52813c5f6e4b0f78caeee7ac39a3ab
bergsonall <bruninhodubal@hotmail.com>
2026-01-08 20:08:50 -0300

    remove db local, config firebase auth e db

---
+++ beckend/auth.js
@@ -0,0 +1,15 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCwHSefApnq82su3wZrSQ5GC4jdmnoiqL0",
+    authDomain: "barbearia-63be3.firebaseapp.com",
+    projectId: "barbearia-63be3",
+    storageBucket: "barbearia-63be3.firebasestorage.app",
+    messagingSenderId: "1018794607068",
+    appId: "1:1018794607068:web:c0f402996b97d90d760f19"
+  };
+  firebase.initializeApp(firebaseConfig);
+
+firebase.auth().signInWithEmailAndPassword("teste@abc.com", "123456").then(response =>{
+        console.log("Usuário logado com sucesso!", response);
+}).catch(error =>{
+    console.error("Erro ao logar o usuário:", error);
+})
